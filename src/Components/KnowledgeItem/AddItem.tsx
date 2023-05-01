70e841334b6a58595d5aa16d56f9e601bf37a60d
MichaelR <spider747@gmail.com>
2023-05-01 21:50:58 +0300

    Add cancel button to close modal

--- src/Components/KnowledgeItem/AddItem.tsx
+++ src/Components/KnowledgeItem/AddItem.tsx
@@ -1,9 +1,16 @@
+import { useAppDispatch } from '../../hooks';
+import { closeModal } from '../../store/modalSlice';
 import Modal from '../UI/Modal';
 
 const AddItem = () => {
+
+  const dispatch = useAppDispatch()
+
   return (
     <Modal>
       <h1>Add Item form</h1>
+      <button>Submit</button>
+      <button onClick={() => dispatch(closeModal())}>Cancel</button>
     </Modal>
   );
 }
